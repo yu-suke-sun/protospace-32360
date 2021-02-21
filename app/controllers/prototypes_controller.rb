792a13d744af9f5d6c1568ed5abe08bbfd32b1fe
yu-suke-sun <76049703+yu-suke-sun@users.noreply.github.com>
2021-02-21 22:53:13 +0900

    アクセス制限の実装部分の編集

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :move_to_session, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
 
   def index
@@ -58,9 +58,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless current_user.id == @prototype.user_id
-      redirect_to root_path
+    prototype = Prototype.find(params[:id])
+    unless current_user.id == prototype.user_id
+      redirect_to action: :index
     end
   end
 
